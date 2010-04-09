a908b7b66f78e89d960aac9ab223fa5713e238f5
KURASHIKI Satoru <lurdan@gmail.com>
2010-04-10 01:54:11 +0900

    add files related to X.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,45 @@
+import XMonad
+
+
+-- import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.SetWMName
+
+myLogHook::X()
+myLogHook = fadeInactiveLogHook fadeAmount
+    where fadeAmount = 0x99999999
+
+myManageHook = composeAll [
+      className =? "Gimp" --> doFloat
+    , className =? "Vlc" --> doFloat
+    , className =? "gxine" --> doFloat
+    , className =? "pidgin" --> doFloat
+               ]
+
+defaultGaps::[(Int,Int,Int,Int)]
+defaultGaps = [(16,0,0,0)]
+
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar"
+  xmonad $ defaultConfig
+		{ manageHook	= manageDocks <+>myManageHook <+>
+                                  manageHook defaultConfig
+		, startupHook	= setWMName "LG3D"
+		, layoutHook	= avoidStruts $ layoutHook defaultConfig
+		, logHook	= (dynamicLogWithPP $ xmobarPP {
+                                    ppOutput = hPutStrLn xmproc
+                                  , ppTitle = xmobarColor "green" "" . shorten 50
+                                  } ) >> myLogHook
+		, modMask	= mod4Mask
+                , terminal = "urxvtc"
+		} `additionalKeys`
+                [((mod4Mask .|. shiftMask, xK_z),
+                  spawn "xscreensaver-command -lock"),
+                 ((controlMask, xK_Print), spawn "scrot -s"),
+                 ((0, xK_Print), spawn "scrot")
+                ]

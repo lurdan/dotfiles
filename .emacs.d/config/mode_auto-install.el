c679bd9c1b9ea49c4d732f598c26dbb2117571f8
KURASHIKI Satoru <lurdan@gmail.com>
2010-08-20 18:57:46 +0900

    cancel save confirmation

--- .emacs.d/config/mode_auto-install.el
+++ .emacs.d/config/mode_auto-install.el
@@ -1,7 +1,8 @@
 ;;;;;;;;;;;;;;;
 ;; auto-install
 (require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/auto-install/")
+(setq auto-install-directory "~/.emacs.d/auto-install/"
+      auto-install-save-confirm nil)
 (auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)
 

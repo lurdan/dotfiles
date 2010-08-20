3066bbf10b9d55ae8024ccfcf4309b1878303367
KURASHIKI Satoru <lurdan@gmail.com>
2010-08-20 19:00:23 +0900

    add install comment

--- .emacs.d/config/mode_auto-save-buffers-enhanced.el
+++ .emacs.d/config/mode_auto-save-buffers-enhanced.el
@@ -1,5 +1,6 @@
 ;;;;;;;;;;;
 ;; 自動保存
+;(install-elisp "http://coderepos.org/share/export/15652/lang/elisp/auto-save-buffers-enhanced/trunk/auto-save-buffers-enhanced.el")
 (require 'auto-save-buffers-enhanced)
 (setq auto-save-buffers-enhanced-interval 2)
 (auto-save-buffers-enhanced t)
